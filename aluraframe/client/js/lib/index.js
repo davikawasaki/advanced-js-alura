0ccd491e88e56317148de44627960c30804482ed
Davi Kawasaki <davishinjik@gmail.com>
2017-12-07 12:02:18 -0200

    Negotiation class + let for index.js

--- aluraframe/client/js/lib/index.js
+++ aluraframe/client/js/lib/index.js
@@ -1,13 +1,13 @@
 
 // Get form fields
-var fields = [
+let fields = [
     document.querySelector('#data'),
     document.querySelector('#quantidade'),
     document.querySelector('#valor')
 ];
 
 // Get table body to insert new registration
-var tbody = document.querySelector('table tbody');
+let tbody = document.querySelector('table tbody');
 
 // When form.form is submitted, insert the registration data into table body
 document.querySelector('.form').addEventListener('submit', function(event) {
@@ -16,17 +16,17 @@ document.querySelector('.form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     // Start a new table body line
-    var tr = document.createElement('tr');
+    let tr = document.createElement('tr');
     
     // Iterate each form field and insert it into a new cell, and then append all cells into tr line
     fields.forEach(function(field) {
-        var td = document.createElement('td');
+        let td = document.createElement('td');
         td.textContent = field.value;
         tr.appendChild(td);
     });
 
     // Create a new cell from quantity times value
-    var tdVolume = document.createElement('td');
+    let tdVolume = document.createElement('td');
     tdVolume.textContent = fields[1].value * fields[2].value;
 
     // Append volume to tr line and then append line to table body
