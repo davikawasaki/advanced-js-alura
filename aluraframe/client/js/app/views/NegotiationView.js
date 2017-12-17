a5dc99b3555e32a2523f8017f19ea5d4c5ccbad4
Davi Kawasaki <davishinjik@gmail.com>
2017-12-17 13:30:55 -0200

    Managing negotiation view class

---
+++ aluraframe/client/js/app/views/NegotiationView.js
@@ -0,0 +1,53 @@
+class NegotiationView {
+
+    constructor(el) {
+        this._el = el;
+    }
+
+    _template(model) {
+        console.log(model)
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                <!-- Iterate negotiations and return concatenated string of td -->
+                ${model.negotiations.map(n => `
+                    <tr>
+                        <td>${DateHelper.date2txt(n.date)}</td>
+                        <td>${n.quantity}</td>
+                        <td>${n.value}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+                `).join('')}
+            </tbody>
+            
+            <tfoot>
+                <td colspan="3"></td>
+                <td>${
+                    // Auto invokable function
+                    // IIFE: Immediate invoked function expression
+                    // @see: https://imasters.com.br/front-end/javascript/sobre-funcoes-imediatas-javascript-iife/?trace=1519021197&source=single
+                    (function() {
+                        let total = 0;
+                        model.negotiations.forEach(n => total += n.volume );
+                        console.log(total)
+                        return total;
+                    })()
+                }</td>
+            </tfoot>
+        </table>
+        `;
+    }
+
+    update(model) {
+        this._el.innerHTML = this._template(model);
+    }
+}
