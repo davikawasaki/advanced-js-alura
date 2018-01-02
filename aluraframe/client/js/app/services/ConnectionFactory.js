67e6d4a3e2c8691e69abbc04188942c4f9a9cf47
Davi Kawasaki <davishinjik@gmail.com>
2018-01-01 23:40:47 -0200

    ConnectionFactory for IndexedDB

---
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,66 @@
+// MODULE PATTERN WITH AUTO INVOKABLE FUNCTION
+// @see: https://medium.com/@tkssharma/javascript-module-pattern-b4b5012ada9f
+
+var ConnectionFactory = (function() {
+    const stores = ['negotiations'];
+    const version = 1;
+    const dbName = 'aluraframe';
+    
+    var connection = null;
+    var close = null;
+    
+    return class ConnectionFactory {
+    
+        // ConnectionFactory with only one accessible connection as a static method
+        // 1. getConnection must be a static method
+        // 2. getConnection must return a promise
+        // 3. connection gotta be the same no matter how many times the static method is called
+        // 4. connection can't be closed directly, which means it can only happens through connectionFactory
+    
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory!');
+        }
+    
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+                openRequest.onsuccess = e => {
+                    if(!connection) {
+                        connection = e.target.result;
+                        // Monkey-patching: changing API logic
+                        // @see: http://me.dt.in.th/page/JavaScript-override/
+                        close = connection.close.bind(connection);   
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar diretamente a conexão!');
+                        }
+                    }
+                    resolve(connection);
+                };
+                openRequest.onerror = e => {
+                    console.error(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+        
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
+
+        static closeConnection() {
+            if(connection) {
+                close();
+                // Option without bind on monkey-patch
+                // Reflect.apply(close, connection, []);
+                connection = null;
+            }
+        }
+    
+    }
+});
